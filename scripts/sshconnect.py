deef3861c3fa3d62d9bf0efff5db3d2420b626a0
user.name : "Rajeshs <rajesh.ramasamy@austrac.gov.au>
2021-03-31 18:51:42 +1100

    readme updates

--- scripts/sshconnect.py
+++ scripts/sshconnect.py
@@ -7,4 +7,5 @@ import sys
 f = open(sys.argv[1])
 yaml_file = yaml.safe_load(f)
 for svr in yaml_file:
-    print os.system("ssh -q vagrant@svr['box']['name']")
+   result = os.system("ssh -q vagrant@svr['box']['name'] exit; echo $? ")
+   print result

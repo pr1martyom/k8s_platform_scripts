379fd02b7b3d7bcf567543f2776e81246ecf1033
user.name : "Rajeshs <rajesh.ramasamy@austrac.gov.au>
2021-03-31 21:32:08 +1100

    updates

--- provisioner.sh
+++ provisioner.sh
@@ -92,7 +92,7 @@ echo "Provisioning Kubernetes VMs"
 cd $WORKING_DIR
 vagrant plugin uninstall vagrant-vbguest
 vagrant plugin install vagrant-vbguest --plugin-version 0.21
-"SIZE=$WORKING_DIR$SIZE" vagrant up
+"SIZE='$WORKING_DIR$SIZE'" vagrant up
 checkssh
 configureHost
 }

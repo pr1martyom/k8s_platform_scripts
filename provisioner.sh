38fa85cc2acf9eacd8c98f75859070d25032e8bd
Rajesh Ramasamy <qzhub@qzhub-dev-01.com>
2021-04-03 02:39:53 +0300

    updates

--- provisioner.sh
+++ provisioner.sh
@@ -52,8 +52,8 @@ mkdir -p /home/qzhub/.venv
 /home/qzhub/.local/bin/virtualenv -p python3 --system-site-packages /home/qzhub/.venv
 source /home/qzhub/.venv/bin/activate
 pip install --upgrade pip
-cd $RUNNER_DIR/kubernetes/kubespray
 pip3 install -r requirements.txt && pip list
+cd $RUNNER_DIR; 
 ansible-playbook -i /home/qzhub/runner/k8s_platform_scripts/scripts/inventory/qzhub/hosts.ini ./cluster.yml -become --become-user=root -i  /home/qzhub/.ssh/id_rsa -e ansible_user=vagrant
 ssh vagrant@kube-master-01 "sudo cat /root/.kube/config" > /tmp/config
 echo "K8s Install Completed..." 
@@ -68,28 +68,32 @@ echo "Installing Ingress Controller.."
 cd $RUNNER_DIR/charts/nginx-ingress
 helm upgrade --debug --install --create-namespace ingress-controller -n ingress-controller --set controller.name=nginx-ingress-controller  --set controller.kind=daemonset --set controller.healthStatus=true --set controller.healthStatusURI="/healthz" --set controller.ingressClass=nginx-controller  --set controller.service.type=NodePort --set controller.service.httpPort.nodePort=32038 --set controller.service.httpsPort.nodePort=32034 --set prometheus.create=true --set controller.service.customPorts[0].port=9113 --set controller.service.customPorts[0].targetPort=9113 --set controller.service.customPorts[0].protocol=TCP --set controller.service.customPorts[0].name=ingress-prometheus --set controller.service.customPorts[0].nodePort=31040 --set-string controller.config.entries.use-forward-headers=true,controller.config.entries.compute-full-forwarded-for=true,controller.config.entries.use-proxy-protocol=true .
 
+
+echo "Installing local storage provisioner"
+cd $RUNNER_DIR/charts/local-provisioner
+kubectl apply -f local-path-storage.yaml
+echo "Install Charts Completed..." 
+
+
 echo "Installing Smoketest.."
 cd $RUNNER_DIR/charts/helm-smoketest
 helm upgrade --debug --install --create-namespace smoketest -n smoketest --set image.repository=sohnaeo/nginx-php-http-header --set image.tag=1.11 --set ingressexternalClass.name=nginx-controller --set ingress.external.hosts[0]=smoketest.qzhub.kz  --set kubernetes.version=1.19.7 --set kubernetes.nginxingressVersion=1.19.6 --set kubernetes.etcdVersion=3.4.13 --set kubernetes.calicoVersion=3.16.5 --set kubernetes.dockerVersion=1.13.1 --set kubernetes.helmVersion=3.3.4 .
 
+
 echo "Installing Kubeview.."
 cd $RUNNER_DIR/charts/kubeview 
 helm upgrade --debug --install --create-namespace kubeview -n kubeview --set ingress.hosts[0].host=kubeview.qzhub.kz --set-string ingress.hosts[0].paths[0]="/" --set ingress.className=nginx-controller .
+
 
 echo "Installing Kubernetes dashboard.."
 cd $RUNNER_DIR/charts/k8s-dashboard
 kubectl apply -f $RUNNER_DIR/charts/k8s-dashboard/recommended.yaml
 kubectl apply -f $RUNNER_DIR/charts/k8s-dashboard/k8s-dashboard-ing.yaml
 
+
 echo "Installing Prometheus/Grafna.."
 cd $RUNNER_DIR/charts/kube-prometheus-stack
 helm upgrade --debug --install --create-namespace monitoring -n monitoring .
-
-
-echo "Installing local storage provisioner"
-cd $RUNNER_DIR/charts/local-provisioner
-kubectl apply -f local-path-storage.yaml
-echo "Install Charts Completed..." 
 
 }
 
